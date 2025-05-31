c66b3beacf761aca2ec8c8fab51ad2c27369fae5
Isai Medina <isai@zettabrasil.com.br>
2025-05-31 14:45:26 -0300

    feat: ajustado para recuperar la ip de host mediante variables de entorno

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,12 @@ mod generated {
 
 use dotenv::dotenv;
 use generated::storage_service_server::StorageServiceServer;
+use std::{env, process};
 use storage::StorageServiceImpl;
 use tonic::transport::Server;
 use tracing_subscriber;
+
+const ADDR_IPV4: &str = "ADDR_IPV4";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -22,12 +25,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     tracing_subscriber::fmt().with_env_filter("info").init();
 
-    let addr = "[::1]:50051".parse()?;
-    tracing::info!("Starting gRPC server at {}", addr);
+    let addr_ipv4 = match env::var(ADDR_IPV4) {
+        Ok(addr) => addr.parse()?,
+        Err(e) => {
+            eprintln!("Error loading {} env var: {}", ADDR_IPV4, e);
+            process::exit(1);
+        }
+    };
+    tracing::info!("Starting gRPC server at {}", addr_ipv4);
 
     Server::builder()
         .add_service(StorageServiceServer::new(StorageServiceImpl::default()))
-        .serve(addr)
+        .serve(addr_ipv4)
         .await?;
 
     Ok(())
